3cd223589249815aa11fe3a2956f0c4ef95979ef
Alex <95392921+alexandruzero@users.noreply.github.com>
2022-10-31 21:20:07 +0200

    Update YTDownloader-CLI.py
    Audio files now convert to .mp3 from mp4. Checks for video unavailable error.

--- YTDownloader-CLI.py
+++ YTDownloader-CLI.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
-
 from pytube import YouTube
 from datetime import timedelta
+import os
+from moviepy.editor import *
+# To do
+# Some characters dont work when calling them with os.exists, os.remove, etc. ($)
+# Get rid of filename_prefix?
+# Catch wrong youtube link error
 
-
-def yt_choice():
+while True:
     url = input("Enter the youtube URL: ")
     yt = YouTube(url)
     # Info about the video
@@ -17,24 +21,45 @@ def yt_choice():
     print('Title: ' + str(yt.title))
     print('Date published: ' + str(yt.publish_date))
     print('Length: ' + "{:0>8}".format(str(timedelta(seconds=int(yt.length)))))
-    # print('Length: ' + str("%.2f" % float(yt.length) / 60))
     print('Views: ' + str(yt.views))
     print('Age Restricted: ' + str(yt.age_restricted))
+    # Video not available issue
+    audio_err = '1Video Not Available.mp4'
+    video_err = '2Video Not Available.mp4'
+    error_found = 'The file failed to download. Retrying.....'
     # Download choices
+    full_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop/')  # Generic desktop path
     choice = input('[1]Audio/[2]Video/[3]Exit: ')
+
     if choice == '1':
         print('Downloading...')
-        YouTube(url).streams.get_audio_only().download('/home/alex/Desktop/', filename_prefix='1')
-        print('Audio downloaded.' + '\n')
+        audio_download = YouTube(url).streams.get_audio_only().download(full_path, filename_prefix='1')
+        # Checks if the file downloaded correctly
+        if os.path.exists(full_path + '1' + str(yt.title) + '.mp4'):
+            print('Audio file downloaded.' + '\n' + 'Converting to .mp3 format.....')
+            convert_mp3 = AudioFileClip(full_path + '1' + str(yt.title) + '.mp4')     
+            convert_mp3.write_audiofile(full_path + '1' + str(yt.title) + '.mp3')     
+            convert_mp3.close()
+            os.remove(full_path + '1' + str(yt.title) + '.mp4')
+        elif os.path.exists(full_path + audio_err):
+            os.remove(full_path + audio_err)
+            print(error_found)
+            audio_download
+            print('Audio file downloaded.' + '\n')
+
     elif choice == '2':
         print('Downloading...')
-        YouTube(url).streams.get_highest_resolution().download('/home/alex/Desktop/', filename_prefix='2')
-        print('Video downloaded.' + '\n')
+        video_download = YouTube(url).streams.get_highest_resolution().download(full_path, filename_prefix='2')
+        # Same ^^
+        if os.path.exists(full_path + '2' + str(yt.title) + '.mp4'):
+            print('Video file downloaded.' + '\n')
+        elif os.path.exists(full_path + video_err):
+            os.remove(full_path + video_err)
+            print(error_found)
+            video_download
+            print('Video file downloaded.' + '\n')
+
     elif choice == '3':
-        return
+        break
     else:
         print("Try again." + '\n')
-    yt_choice()
-
-
-yt_choice()
